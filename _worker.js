358031842f612f8a578fb87a32807e8e107b1a78
Mryiwen <40563921+Mryiwen@users.noreply.github.com>
2024-11-04 04:26:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "01da639c-2d08-7a3c-8922-73e584260db1";
+var userID = "b15fb69f-ed6f-4784-9045-670ad1178f9b";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
